f085c60a9ef3770991e72e796c9ebb6b22c9616b
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-02 11:28:22 +0330

    Delete fifth test .skip statement & passed it

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function (string, number) {
   let output = "";
+
+  if (number < 0) return "ERROR";
+
   for (let i = 0; i < number; i++) {
     output += string;
   }
@@ -10,6 +13,7 @@ repeatString("hey", 3);
 repeatString("hello", 10);
 repeatString("hi", 1);
 repeatString("bye", 0);
+repeatString("goodbye", -1);
 
 // Do not edit below this line
 module.exports = repeatString;
