bff1aa5b92bbe21a4419a624bc5f67a7e19df57e
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-02 11:58:32 +0330

    Add reverseString code & passed first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const stringLetters = string.split("");
+  const reverseStringLetters = stringLetters.reverse();
+  const resultString = reverseStringLetters.join("");
+  return resultString;
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
