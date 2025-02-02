f64b03a096db4e3f120786bc91073b8c687d4a59
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-02 11:12:46 +0330

    Add code to repeatString & passed first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let output = "";
+  for (let i = 0; i < number; i++) {
+    output += string;
+  }
+  return output;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
