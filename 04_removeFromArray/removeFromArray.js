88477aa77e3e84038985f1f2de4814bd9681e690
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-03 11:30:00 +0330

    Add removeFromArray code & passed first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  const newArray = [];
+  for(const element of array) {
+    if(!args.includes(element)) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
