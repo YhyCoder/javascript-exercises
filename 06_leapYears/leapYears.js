026dc3ecc3e92891ce00653efd71875adb2afd04
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-03 19:23:24 +0330

    Solve all exercise 06_leapYears tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const divisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const divisibleByFourHundred = year % 400 === 0;
 
+  if (divisibleByFour && !isCentury || divisibleByFourHundred) {
+    return true;
+  }
+  return false;
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
