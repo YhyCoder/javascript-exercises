1716e00195d25fb3c4f637bc3695bf35b06bf287
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-03 18:43:20 +0330

    Solve all exercise 05_sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (minNumber, maxNumber) {
+  let sum = 0;
 
+  if (
+    minNumber < 0 ||
+    maxNumber < 0 ||
+    !Number.isInteger(minNumber) ||
+    !Number.isInteger(maxNumber)
+  ) {
+    return "ERROR";
+  }
+
+  if (minNumber > maxNumber) {
+    const temporary = minNumber;
+    minNumber = maxNumber;
+    maxNumber = temporary;
+  }
+
+  for (minNumber; minNumber <= maxNumber; minNumber++) {
+    sum += minNumber;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
