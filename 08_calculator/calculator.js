302671ad27721556765b08a70f547be6511f6b71
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-22 11:24:47 +0330

    Write subtract function code & solve it tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -2,8 +2,8 @@ const add = function(firstNumber, secondNumber) {
 	return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
 };
 
 const sum = function() {
