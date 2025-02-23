0f751722400d2c8b2be958c2a9651cb8f77175c8
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-23 11:54:07 +0330

    Solved all test of 10 exercise (fibonacci)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let fn_1 = 1;
+  let fn_2 = 0;
 
+  if (number < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 0; i < number; i++) {
+    let fn = fn_1 + fn_2;
+    fn_2 = fn_1;
+    fn_1 = fn;
+  }
+  return fn_2;
 };
 
 // Do not edit below this line
