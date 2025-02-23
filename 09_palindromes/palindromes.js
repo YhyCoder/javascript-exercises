2d222e282e60a6987ef71cbee8e3e1e1e314f440
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-23 11:09:41 +0330

    Solve 09 exercise tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphabetNumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cleanPunctuation = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphabetNumerical.includes(character))
+    .join("");
 
+    const reverseString = cleanPunctuation.split("").reverse().join("");
+
+    return cleanPunctuation === reverseString;
 };
+
+console.log(palindromes("racecar!"));
 
 // Do not edit below this line
 module.exports = palindromes;
