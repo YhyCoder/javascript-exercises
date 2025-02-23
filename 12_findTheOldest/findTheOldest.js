0b46b26f9410cd2e2dcc11daf5ca6da813e499c0
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-23 12:49:15 +0330

    Solve all tests of 12 exercise (findTheOldest)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  let result = array
+    .map((person) => {
+      if (!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+      }
 
+      return {
+        name: person.name,
+        age: person.yearOfDeath - person.yearOfBirth,
+      };
+    })
+    .sort((a, b) => b.age - a.age);
+  return result[0];
 };
+
+// const people = [
+//   {
+//     name: "Carly",
+//     yearOfBirth: 1942,
+//     yearOfDeath: 1970,
+//   },
+//   {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//   },
+//   {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//   },
+// ]
+
+// console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
